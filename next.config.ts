de88a96701698ff48a23d6211ceae9fce3ab6d43
TommasoTricker <trickertommaso@gmail.com>
2025-05-16 01:20:29 +0200

    added basePath and assetPrefix

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/latex-playground",
+  assetPrefix: "/latex-playground/",
 }
 
 export default nextConfig
