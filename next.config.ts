7fb46b4412be7cf26c5c3b59815738a8ac70067d
TommasoTricker <trickertommaso@gmail.com>
2025-05-16 01:08:34 +0200

    fix GitHub pages setup

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/latex-playground",
-  assetPrefix: "/latex-playground/",
 }
 
 export default nextConfig
