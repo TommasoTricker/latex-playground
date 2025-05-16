3c1e0ca7f0f8bc24c793616a2dcf64b7c111fb15
TommasoTricker <trickertommaso@gmail.com>
2025-05-16 02:22:54 +0200

    added isProd check to fix npm run dev

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,12 @@
 import type { NextConfig } from "next"
 
+const isProd = process.env.NODE_ENV === "production"
+
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/latex-playground",
-  assetPrefix: "/latex-playground/",
-  trailingSlash: true
+  basePath: isProd ? "/latex-playground" : "",
+  assetPrefix: isProd ? "/latex-playground/" : "",
+  trailingSlash: true,
 }
 
 export default nextConfig
