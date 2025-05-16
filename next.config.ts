176e2a24cbd8ae6106e639594144b817a91b6495
TommasoTricker <trickertommaso@gmail.com>
2025-05-16 16:42:02 +0200

    remove useless assetPrefix

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,6 @@ const isProd = process.env.NODE_ENV === "production"
 const nextConfig: NextConfig = {
   output: "export",
   basePath: isProd ? "/latex-playground" : "",
-  assetPrefix: isProd ? "/latex-playground/" : "",
   trailingSlash: true,
 }
 
